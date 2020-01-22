217111fe103e56a75bf00c6a00a91a2b31210b76
Darrin Eden <deden@digitalocean.com>
2020-01-22 14:10:54 -0800

    a second twitter swing

--- go.mod
+++ go.mod
@@ -5,5 +5,5 @@ go 1.13
 require (
 	github.com/aws/aws-lambda-go v1.13.3
 	github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f
-	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+	github.com/dghubble/oauth1 v0.6.0
 )
--- main.go
+++ main.go
@@ -2,35 +2,63 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/dghubble/go-twitter/twitter"
-
-	"golang.org/x/oauth2/clientcredentials"
+	"github.com/dghubble/oauth1"
 )
 
 func handler(_ events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	config := &clientcredentials.Config{
-		ClientID:     os.Getenv("TWITTER_CLIENT_ID"),
-		ClientSecret: os.Getenv("TWITTER_CLIENT_SECRET"),
-		TokenURL:     "https://api.twitter.com/oauth2/token",
-	}
-	httpClient := config.Client(context.Background())
+	config := oauth1.NewConfig(
+		os.Getenv("TWITTER_CLIENT_ID"),
+		os.Getenv("TWITTER_CLIENT_SECRET"),
+	)
+	token := oauth1.NewToken(
+		os.Getenv("TWITTER_ACCESS_TOKEN"),
+		os.Getenv("TWITTER_ACCESS_SECRET"),
+	)
 
+	httpClient := config.Client(context.Background(), token)
 	client := twitter.NewClient(httpClient)
-	homeTimelineParams := &twitter.HomeTimelineParams{}
 
-	tweets, resp, err := client.Timelines.HomeTimeline(homeTimelineParams)
+	userTimelineParams := &twitter.UserTimelineParams{}
+
+	tweets, resp, err := client.Timelines.UserTimeline(userTimelineParams)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	threeDaysAgo := time.Now().AddDate(0, 0, -2)
+	destroyed := 0
+
+	for _, tweet := range tweets {
+		tweetTime, _ := time.Parse(time.RubyDate, tweet.CreatedAt)
+		if tweetTime.Before(threeDaysAgo) {
+			if tweet.Retweeted {
+				_, _, err := client.Statuses.Unretweet(tweet.ID, &twitter.StatusUnretweetParams{})
+				if err != nil {
+					log.Print(err)
+				}
+				destroyed++
+			} else {
+				_, _, err := client.Statuses.Destroy(tweet.ID, &twitter.StatusDestroyParams{})
+				if err != nil {
+					log.Print(err)
+				}
+				destroyed++
+			}
+		}
+
+	}
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: resp.StatusCode,
-		Body:       tweets[0].FullText,
+		Body:       fmt.Sprintf("%d tweets deleted", destroyed),
 	}, nil
 }
 
