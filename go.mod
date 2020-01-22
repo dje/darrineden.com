c01670db012c366a557ea2166a602b9b2a0e1e62
Darrin Eden <deden@digitalocean.com>
2020-01-22 12:20:36 -0800

    first swing at twitter

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/dje/darrineden.com
+
+go 1.13
+
+require (
+	github.com/aws/aws-lambda-go v1.13.3
+	github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
+)
--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/dghubble/go-twitter/twitter"
+
+	"golang.org/x/oauth2/clientcredentials"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func handler(_ events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	config := &clientcredentials.Config{
+		ClientID:     os.Getenv("TWITTER_CLIENT_ID"),
+		ClientSecret: os.Getenv("TWITTER_CLIENT_SECRET"),
+		TokenURL:     "https://api.twitter.com/oauth2/token",
+	}
+	httpClient := config.Client(context.Background())
+
+	client := twitter.NewClient(httpClient)
+	homeTimelineParams := &twitter.HomeTimelineParams{}
+
+	tweets, resp, err := client.Timelines.HomeTimeline(homeTimelineParams)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello AWS Lambda and Netlify",
+		StatusCode: resp.StatusCode,
+		Body:       tweets[0].FullText,
 	}, nil
 }
 
