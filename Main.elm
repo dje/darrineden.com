a760b4455adb26c8b3373651e7f203892d66fcd1
Darrin Eden <deden@digitalocean.com>
2020-01-23 11:45:07 -0800

    first swing at switching to Elm

---
+++ Main.elm
@@ -0,0 +1,68 @@
+module Main exposing (main)
+
+import Browser exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = Nothing
+
+
+type alias Flags =
+    ()
+
+
+main : Program Flags Model Msg
+main =
+    document
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( {}, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update _ _ =
+    ( {}, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+view : Model -> Document Msg
+view _ =
+    { title = "Darrin Eden"
+    , body =
+        [ div []
+            [ h1 [] [ text "Hi There, Nice to meet you." ]
+            , p [] [ text "Want to know more about me? Here are some places to connect:" ]
+            , ul []
+                [ li [] [ a [ href "https://twitter.com/DarrinEden" ] [ text "Twitter" ] ]
+                , li [] [ a [ href "https://www.linkedin.com/in/darrin-eden-99b65719b/" ] [ text "LinkedIn" ] ]
+                , li [] [ a [ href "https://www.instagram.com/darrin.eden/" ] [ text "Instagram" ] ]
+                , li [] [ a [ href "mailto:darrin.eden@gmail.com" ] [ text "darrin.eden@gmail.com" ] ]
+                , li [] [ a [ href "https://github.com/dje" ] [ text "GitHub" ] ]
+                ]
+            , p [] [ text "Thanks for visiting!" ]
+            , p []
+                [ text "Sincerely,"
+                , br [] []
+                , text "Darrin"
+                ]
+            , p [] [ text "Last update: 2020/01/17" ]
+            ]
+        ]
+    }
--- Makefile
+++ Makefile
@@ -1,3 +1,11 @@
-build:
+build : build-elm build-functions
+
+build-elm :
+	elm make Main.elm --output=site/elm.js
+
+build-functions :
 	mkdir -p functions
 	go build -o functions/delete-tweets ./...
+
+clean :
+	rm -r site/elm.js functions
