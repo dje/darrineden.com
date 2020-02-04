3f5fa2e8eb508f1f5778a2e325e6121753f8989a
Darrin Eden <darrin.eden@gmail.com>
2020-02-04 11:08:30 -0800

    Remove email link

--- Main.elm
+++ Main.elm
@@ -54,7 +54,6 @@ view _ =
                 , li [] [ a [ href "https://www.linkedin.com/in/darrin-eden-99b65719b/" ] [ text "LinkedIn" ] ]
                 , li [] [ a [ href "https://storage.googleapis.com/darrineden/Darrin-Eden-Resume.pdf" ] [ text "Résumé" ] ]
                 , li [] [ a [ href "https://www.instagram.com/darrin.eden/" ] [ text "Instagram" ] ]
-                , li [] [ a [ href "mailto:darrin.eden@gmail.com" ] [ text "darrin.eden@gmail.com" ] ]
                 , li [] [ a [ href "https://github.com/dje" ] [ text "GitHub" ] ]
                 ]
             , p [] [ text "Thanks for visiting!" ]
