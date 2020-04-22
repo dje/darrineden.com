db6e8ce3eadca425dd3fbfabdc3a90d74d905506
Darrin Eden <darrin.eden@gmail.com>
2020-04-21 17:17:02 -0700

    event bridge hourly

--- aws/app.py
+++ aws/app.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 
 from aws_cdk import (core,
-                     aws_apigateway as apigw,
+                     aws_events as events,
                      aws_lambda as lambda_,
-                     aws_logs as logs,
-                     aws_ssm as ssm)
+                     aws_ssm as ssm,
+                     aws_events_targets as targets)
 
 
 class DarrinEdenStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-
-        log_group = logs.LogGroup(self, "DeleteTweetsLogGroup",
-                                  removal_policy=core.RemovalPolicy.DESTROY,
-                                  retention=logs.RetentionDays.ONE_WEEK
-                                  )
 
         delete_tweets = lambda_.Function(self, "DeleteTweetsHandler",
                                          runtime=lambda_.Runtime.GO_1_X,
@@ -24,18 +19,16 @@ class DarrinEdenStack(core.Stack):
         [ssm.StringParameter.from_secure_string_parameter_attributes(
             self, f"Twitter{i}Value",
             parameter_name=f"/DeleteTweets/Twitter{i}",
-            version=1
-        ).grant_read(delete_tweets) for i in ["ClientId", "ClientSecret", "AccessToken", "AccessSecret"]]
+            version=1).grant_read(delete_tweets)
+         for i in ["ClientId",
+                   "ClientSecret",
+                   "AccessToken",
+                   "AccessSecret"]
+         ]
 
-        api = apigw.RestApi(self, "delete-tweets",
-                            deploy_options=apigw.StageOptions(
-                                access_log_destination=apigw.LogGroupLogDestination(log_group),
-                                logging_level=apigw.MethodLoggingLevel.INFO
-                            ),
-                            rest_api_name="Delete Tweets",
-                            description="This service deletes tweets.")
-
-        api.root.add_method("GET", apigw.LambdaIntegration(delete_tweets))
+        events.Rule(self, "DeleteTweetsHourlyEvent",
+                    schedule=events.Schedule.cron(minute="0"),
+                    targets=[targets.LambdaFunction(delete_tweets)])
 
 
 app = core.App()
