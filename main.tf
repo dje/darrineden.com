fa6e7f338e260663c6c08a554de09665f807061e
Darrin Eden <darrin.eden@gmail.com>
2020-08-15 13:16:13 -0700

    terraform fmt

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
 
   backend "s3" {
     bucket = "darrineden.com-terraform"
-    key = "terraform-state"
+    key    = "terraform-state"
     region = "us-west-2"
   }
 }
@@ -18,7 +18,7 @@ provider "aws" {
 
 provider "aws" {
   region = "us-east-1"
-  alias = "east"
+  alias  = "east"
 }
 
 resource "aws_route53_zone" "zone" {
@@ -27,39 +27,39 @@ resource "aws_route53_zone" "zone" {
 
 resource "aws_route53_record" "netlify_record" {
   zone_id = aws_route53_zone.zone.zone_id
-  name = "darrineden.com"
-  type = "A"
-  ttl = "300"
+  name    = "darrineden.com"
+  type    = "A"
+  ttl     = "300"
   records = ["104.198.14.52"]
 }
 
 resource "aws_route53_record" "test_record_ipv4" {
   zone_id = aws_route53_zone.zone.zone_id
-  name = "test.darrineden.com"
-  type = "A"
+  name    = "test.darrineden.com"
+  type    = "A"
 
   alias {
-    name = aws_cloudfront_distribution.dist.domain_name
-    zone_id = aws_cloudfront_distribution.dist.hosted_zone_id
+    name                   = aws_cloudfront_distribution.dist.domain_name
+    zone_id                = aws_cloudfront_distribution.dist.hosted_zone_id
     evaluate_target_health = true
   }
 }
 
 resource "aws_route53_record" "test_record_ipv6" {
   zone_id = aws_route53_zone.zone.zone_id
-  name = "test.darrineden.com"
-  type = "AAAA"
+  name    = "test.darrineden.com"
+  type    = "AAAA"
 
   alias {
-    name = aws_cloudfront_distribution.dist.domain_name
-    zone_id = aws_cloudfront_distribution.dist.hosted_zone_id
+    name                   = aws_cloudfront_distribution.dist.domain_name
+    zone_id                = aws_cloudfront_distribution.dist.hosted_zone_id
     evaluate_target_health = true
   }
 }
 
 resource "aws_acm_certificate" "cert" {
-  provider = aws.east
-  domain_name = "darrineden.com"
+  provider          = aws.east
+  domain_name       = "darrineden.com"
   validation_method = "DNS"
 
   subject_alternative_names = ["*.darrineden.com"]
@@ -72,29 +72,29 @@ resource "aws_acm_certificate" "cert" {
 resource "aws_route53_record" "record" {
   for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name = dvo.resource_record_name
+      name   = dvo.resource_record_name
       record = dvo.resource_record_value
-      type = dvo.resource_record_type
+      type   = dvo.resource_record_type
     }
   }
 
   allow_overwrite = true
-  name = each.value.name
-  records = [each.value.record]
-  ttl = 60
-  type = each.value.type
-  zone_id = aws_route53_zone.zone.zone_id
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.zone.zone_id
 }
 
 resource "aws_acm_certificate_validation" "validation" {
-  provider = aws.east
+  provider        = aws.east
   certificate_arn = aws_acm_certificate.cert.arn
 
   validation_record_fqdns = [for record in aws_route53_record.record : record.fqdn]
 }
 
 resource "aws_s3_bucket" "site" {
-  acl = "public-read"
+  acl    = "public-read"
   bucket = "darrineden.com"
 
   website {
@@ -109,18 +109,18 @@ locals {
 resource "aws_cloudfront_distribution" "dist" {
   origin {
     domain_name = aws_s3_bucket.site.bucket_regional_domain_name
-    origin_id = local.s3_origin_id
+    origin_id   = local.s3_origin_id
   }
 
-  enabled = true
-  is_ipv6_enabled = true
+  enabled             = true
+  is_ipv6_enabled     = true
   default_root_object = "index.html"
 
   aliases = ["darrineden.com", "test.darrineden.com"]
 
   default_cache_behavior {
-    allowed_methods = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods = ["GET", "HEAD"]
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
     target_origin_id = local.s3_origin_id
 
     forwarded_values {
@@ -132,9 +132,9 @@ resource "aws_cloudfront_distribution" "dist" {
     }
 
     viewer_protocol_policy = "allow-all"
-    min_ttl = 0
-    default_ttl = 3600
-    max_ttl = 86400
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
   restrictions {
@@ -145,6 +145,6 @@ resource "aws_cloudfront_distribution" "dist" {
 
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate_validation.validation.certificate_arn
-    ssl_support_method = "sni-only"
+    ssl_support_method  = "sni-only"
   }
 }
