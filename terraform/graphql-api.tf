523232be16e4892e2ed268f31855bfb938ba9e34
Darrin Eden <darrin.eden@gmail.com>
2020-09-15 16:52:15 -0700

    initial graphql api

---
+++ terraform/graphql-api.tf
@@ -0,0 +1,13 @@
+resource "aws_cognito_user_pool" "pool" {
+  name = "api"
+}
+
+resource "aws_appsync_graphql_api" "carbon" {
+  authentication_type = "AMAZON_COGNITO_USER_POOLS"
+  name                = aws_cognito_user_pool.pool.name
+
+  user_pool_config {
+    default_action = "DENY"
+    user_pool_id   = aws_cognito_user_pool.pool.id
+  }
+}
