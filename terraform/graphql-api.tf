ec554b3d1a6e4f3af2a2358d0f272487f5ae754e
Darrin Eden <darrin.eden@gmail.com>
2020-09-15 19:03:59 -0700

    initial schema

--- terraform/graphql-api.tf
+++ terraform/graphql-api.tf
@@ -5,6 +5,7 @@ resource "aws_cognito_user_pool" "pool" {
 resource "aws_appsync_graphql_api" "carbon" {
   authentication_type = "AMAZON_COGNITO_USER_POOLS"
   name                = aws_cognito_user_pool.pool.name
+  schema              = file("../carbon.graphql")
 
   user_pool_config {
     default_action = "DENY"
