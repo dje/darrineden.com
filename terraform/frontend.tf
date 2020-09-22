a911ed5d1f1de78b6ae281e1ccf33b60f740aca0
Darrin Eden <darrin.eden@gmail.com>
2020-09-21 17:44:56 -0700

    redirect to https

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -36,7 +36,7 @@ resource "aws_cloudfront_distribution" "dist" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
