0abb102daad837d8a58147ca0d1364877764d2a8
Darrin Eden <darrin.eden@gmail.com>
2022-06-18 12:58:14 -0700

    Update Recruiting.tsx

--- src/Recruiting.tsx
+++ src/Recruiting.tsx
@@ -3,12 +3,12 @@ import React from "react"
 export const Recruiting = () =>
     <div>
         <p>Hello,</p>
-        <p>Thank you for reaching out. My apologies for this canned response. Out of respect for your time I'd like to share some facts to help self-select which conversations should move forward:</p>
+        <p>Thank you for reaching out. My apologies for this canned response. Out of respect for your time I'd like to share some points to help self-select which conversations should move forward:</p>
         <ol>
-            <li>My salary is in the $200k to $300k range. I am not considering salaries below this band.<br />Equity as part of total compensation is a separate conversation.</li>
-            <li>I believe "leetcode" (puzzle style) perfomative coding steps are a poor predictor of overall on-the-job performance. Instead it selects for those with time to practice and managed performance anxiety. I'm searching for companies thinking beyond this gatekeeping practice. Let's analyze an open-source project or debug a challenging problem instead?</li>
+            <li>My salary is currently in the $200k to $300k range. Opportunities with a significant social good mission are an exception to this requirement. Equity as part of total compensation is a separate conversation.</li>
+            <li>I believe "leetcode" (puzzle style) perfomative coding is a poor predictor of on-the-job performance. Instead it selects those who have managed their performance anxiety and have time to practice. I'm searching for companies thinking beyond this gatekeeping mechanism. Let's analyze an open-source project or debug a challenging problem instead.</li>
             <li>I am only considering remote or Portland, Oregon.</li>
         </ol>
-        <p>If you'd still like to learn more about me after understanding this baseline please schedule an introductory phone call via my <a href="https://calendly.com/darrin-eden/introduction">calendar</a>. Please review my <a href="https://storage.googleapis.com/darrineden/Darrin_Eden_Resume.pdf">resume</a> ahead of time. Thank you for your time and consideration.</p>
+        <p>If you'd still like to learn more about me please schedule an introductory phone call via my <a href="https://calendly.com/darrin-eden/introduction">calendar</a>. Please review my <a href="https://storage.googleapis.com/darrineden/Darrin_Eden_Resume.pdf">resume</a> ahead of time. Thank you for your time and consideration.</p>
         <p>Sincerely,<br />Darrin Eden</p>
     </div>
