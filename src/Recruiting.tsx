2027f7e546ae71861b7bd65059975ef45e86804b
Darrin Eden <darrin.eden@gmail.com>
2022-06-18 13:07:24 -0700

    Update Recruiting.tsx

--- src/Recruiting.tsx
+++ src/Recruiting.tsx
@@ -7,7 +7,7 @@ export const Recruiting = () =>
         <ol>
             <li>My salary is currently in the $200k to $300k range. Opportunities with a significant social good mission are an exception to this requirement. Equity as part of total compensation is a separate conversation.</li>
             <li>I believe "leetcode" (puzzle style) performative coding is a poor predictor of on-the-job performance. Instead it selects those who have managed their performance anxiety and have time to practice. I'm searching for companies thinking beyond this gatekeeping mechanism. Let's analyze an open-source project or debug a challenging problem instead.</li>
-            <li>I am only considering remote or Portland, Oregon.</li>
+            <li>I live in Portland, Oregon and not considering relocation. I've worked remotely for the majority of my career.</li>
         </ol>
         <p>If you'd still like to learn more about me please schedule an introductory phone call via my <a href="https://calendly.com/darrin-eden/introduction">calendar</a>. Please review my <a href="https://storage.googleapis.com/darrineden/Darrin_Eden_Resume.pdf">resume</a> ahead of time. Thank you for your time and consideration.</p>
         <p>Sincerely,<br />Darrin Eden</p>
