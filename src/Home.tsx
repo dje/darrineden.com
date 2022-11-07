5d0c5d88bd9359291ad8e8443fa51504072a91c0
Darrin Eden <darrin.eden@gmail.com>
2022-11-07 12:14:52 -0800

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -9,7 +9,7 @@ export const Home = () =>
             <li><a href="https://github.com/dje">GitHub</a></li>
             <li><a href="https://storage.googleapis.com/darrineden/Darrin_Eden_Resume.pdf">Resume</a></li>
             <li><a href="https://www.linkedin.com/in/darrin-eden">LinkedIn</a></li>
-            <li><a href="https://tinnies.club/@dje">Mastodon</a></li>
+            <li><a href="https://tinnies.club/@dje" rel="me">Mastodon</a></li>
             <li><s><a href="https://twitter.com/DarrinEden">Twitter</a></s></li>
             <li><a href="https://www.instagram.com/darrin.eden">Instagram</a></li>
         </ul>
